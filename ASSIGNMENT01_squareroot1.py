57a7fd1e67b86a9a6059842f0a21c94a5e4a2fec
Nilesh002 <105161049+Nilesh002@users.noreply.github.com>
2023-08-20 00:20:22 +0530

    Add files via upload

---
+++ ASSIGNMENT01_squareroot1.py
@@ -0,0 +1,4 @@
+a=int(input("Enter the number: "))
+
+print(a**0.5)
+
---
+++ ASSIGNMENT01_subtracting.py
@@ -0,0 +1,6 @@
+a=int(input("Enter the 1st number: "))
+b=int(input("Enter the 2nd number: "))
+c=int(input("Enter the 3rd number: "))
+
+print(a-b)
+print(a-b-c)
