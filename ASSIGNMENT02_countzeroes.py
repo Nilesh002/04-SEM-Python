34fbe1570a89075471831151e457fea05dbf8b0a
Nilesh002 <105161049+Nilesh002@users.noreply.github.com>
2023-08-19 23:59:03 +0530

    ASSIGNMENT02_countzeroes.py

--- ASSIGNMENT02_countzeroes.py
+++ ASSIGNMENT02_countzeroes.py
@@ -1,4 +1,4 @@
-# Write a program tocount the no. of one's present in the tuple
+# Write a program to count the no. of one's present in the tuple
 
 b1=int(input("Enter the number: "))
 b2=int(input("Enter the number: "))
